00582f3f520a55edf37d62f22c6b3b590e568ae9
btheado <brian.theado@gmail.com>
2017-07-08 21:47:01 -0400

    Fixed double html escaping issue in markdown code blocks and removed explicit highlightjs dependency

--- src/plugins/btheado/markdown/wrapper.js
+++ src/plugins/btheado/markdown/wrapper.js
@@ -14,7 +14,6 @@ Wraps up the markdown-it parser for use in TiddlyWiki5
 
 var markdown = require("$:/plugins/btheado/markdown/markdown-it.js");
 var WikiParser = require("$:/core/modules/parsers/wikiparser/wikiparser.js")["text/vnd.tiddlywiki"];
-var hljs = require("$:/plugins/tiddlywiki/highlight/highlight.js");
 
 /*
  * The TW markdown parser at http://tiddlywiki.com/plugins/tiddlywiki/markdown/ uses the markdown-js library
@@ -29,31 +28,29 @@ var hljs = require("$:/plugins/tiddlywiki/highlight/highlight.js");
  */
 var MarkdownParser = function(type,text,options) {
   var md, html, myparser;
-  if (hljs) {
-    md = new markdown({
-      "highlight": function (str, lang) {
-        if (lang && hljs.getLanguage(lang)) {
-          try {
-            return '<pre class="hljs"><code>' +
-                   hljs.highlight(lang, str, true).value +
-                   '</code></pre>';
-          } catch (__) {}
-        }
+  md = new markdown();
 
-        return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
-      }
-    });
-  } else {
-    md = new markdown();
+  // Replace the codeblock rule to just return a tiddlywiki codeblock which
+  // will be parsed with the TW parser. This is to get the highlightjs code
+  // highlighting to work. I tried calling highlightjs directly but it
+  // resulted in extra html escaping I couldn't figure out. By doing it this
+  // way, the normal TW highlightjs plugin can be used without having to explicitly
+  // require it here.
+  md.renderer.rules.fence = function(tokens, idx) {
+    var token = tokens[idx];
+    return "\n```" + token.info + "\n" + token.content + "\n```\n";
   }
 
   // Render the markdown to html and create a TW parser
   html = md.render(text),
   myparser = Object.create(WikiParser.prototype);
 
-  // Remove everything except for html parsing
+  // Remove all rules except for html parsing and codeblock
   myparser.pragmaRuleClasses = {};
-  myparser.blockRuleClasses = {"html": myparser.blockRuleClasses.html};
+  myparser.blockRuleClasses = {
+    "codeblock": myparser.blockRuleClasses.codeblock,
+    "html": myparser.blockRuleClasses.html
+  };
   myparser.inlineRuleClasses = {"html": myparser.inlineRuleClasses.html};
 
   // Call the constructor and save the parse tree results
